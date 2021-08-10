01c0e0a8c37a00469d7df369e045c91285aa0922
Lohithaksh <83767927+lohithakshAFK@users.noreply.github.com>
2021-08-10 16:05:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+Webcam.set({
+    height:300,
+    width:350,
+    image_format:"png",
+    png_quality:90
+});
+camera = document.getElementById("camera");
+Webcam.attach("camera");
+
+function snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
+    });
+}
